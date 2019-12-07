6cdec53289cf863b497710fbe34cfbd4627c6c6f
League Student <league@iMac-29.attlocal.net>
2019-12-06 18:28:33 -0800

    ThewrongWaycow (ip)

--- src/extras/the_wrong_way_cow/TheWrongWayCow.java
+++ src/extras/the_wrong_way_cow/TheWrongWayCow.java
@@ -43,18 +43,70 @@ package extras.the_wrong_way_cow;
 
 public class TheWrongWayCow {
 
-    public static int[] findWrongWayCow(final char[][] field) {
+	public static int[] findWrongWayCow(final char[][] field) {
         // Fill in the code to return the x,y coordinate position of the
         // head (letter 'c') of the wrong way cow!
+		int side = 0;
+		int up = 0;
+		int down = 0;
+		int normal = 0;
+    	int[] location1 = new int[2];
+    	int[] location2 = new int[2];
+    	int[] location3 = new int[2];
+    	int[] location4 = new int[2];
         for(int i =0; i < field.length-1; i++) {
         	for(int j = 0; j < field.length-1; j++) {
-        		if(field[i][j] = ) {
-        		if(!field.toString().charAt(i)== 'c' && field.toString().charAt(i+1) == 'o' && ) {
+        		
+        		if(i  < field.length-2) {
         			
+        		if((field[i][j] == 'w') && (field[i+1][j] == 'o') && field[i+2][j] == 'c') {
+        			System.out.println(i + ", " + j + "(condition: flipped side");
+        			side = side +1;
+        			location1[0] = i;
+        			location1[1] = j;
         		}
         		}
+        		else if(i > 1) {
+        			if(field[i][j] == 'c' && field[i-1][j] == 'o' && field[i-2][j] == 'w') {
+        				System.out.println(i + ", " + j + "(condition: normal");
+        				normal = normal +1;
+        				location2[0] = i;
+            			location2[1] = j;
+        			}
+        		}
+        		
+        		else if( j> 1 ) {
+        			 if(field[i][j] == 'c' && field[i][j-1] == 'o' && field[i][j-2] == 'w') {
+        				 System.out.println(i + ", " + j + "(condition: bottom to top");
+        				up = up +1;
+        				location3[0] = i;
+            			location3[1] = j;
+            		}
+        		}
+        		
+        		else if( j < field.length-2) {
+        			if(field[i][j] == 'c' && field[i][j+1] == 'o' && field[i][j+2] == 'w') {
+        				 System.out.println(i + ", " + j + "(condition: top to bottom");
+       				down = down +1;
+       				location4[0] = i;
+        			location4[1] = j;
+           		}
+        		}	 
         	}
         }
-        return null;
-    }
+        if(side == 1) {
+        	return location1;
+        }
+        else if(normal == 1) {
+        	return location2;
+        }
+        else if(up == 1) {
+        	return location3; 
+        }
+        else if(down ==1) {
+        	return location4;
+        }
+    
+return null;
 }
+	}
